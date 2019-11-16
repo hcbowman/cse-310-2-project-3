7b37e8b0a6d970a49eb390b93ee1348f521532c5
hunterbowman <hbowman877@hotmail.com>
2019-11-16 09:34:44 -0700

    data plus makefile

---
+++ code/main.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+
+void get_contents() {
+
+
+
+}
+
+int main() {
+
+    float data;
+
+    std::ifstream file("CS310_project_subregion/1990/Beaufort_Sea_diffw01y1990+landmask", std::ios::in);
+
+    for() {
+        
+    }
+
+
+    return 0;
+}

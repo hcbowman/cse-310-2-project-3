194423aa40f60a9b6234440e185313aaca5ce4b4
Hunter Bowman <hbowman877@hotmail.com>
2019-11-23 17:21:40 -0700

    bug fix + added .vscode files
    -will parse out land and missing data
    -added files for debugging

--- code/main.cpp
+++ code/main.cpp
@@ -6,36 +6,72 @@
 
 #define DATA_SIZE sizeof(float)
 
-void get_contents() {
+void get_contents(std::vector<float> &data) {
 
-
-
-}
-
-int main() {
-
-    float data;
+    float temp;
+    int total;
+    int ice;
+    int land;
+    int missing;
 
     std::ifstream file("CS310_project_subregion/1990/Beaufort_Sea_diffw01y1990+landmask", std::ios::in);
     //std::ifstream file("CS310_project_subregion/recitationExercise", std::ios::in);
 
 
+    //Gets file length
     file.ignore( std::numeric_limits<std::streamsize>::max() );
     std::streamsize length = file.gcount();
-    file.clear();   //  Since ignore will have set eof.
+    file.clear();   //Since ignore will have set eof.
     file.seekg( 0, std::ios_base::beg );
+    //std::cout << "length=" << length << "\n"; //DEBUG
 
-    std::cout << "length=" << length << "\n";
+    for(unsigned int i = 0; i < (length/DATA_SIZE); i++) {
 
-    for(int i = 0; i < (length/DATA_SIZE); i++) {
 
-        file.read( (char*)&data, sizeof(data) );
-        std::cout << "data: "<< data << "\n";
+
+        file.read( (char*)&temp, sizeof(temp) );
+        //std::cout << "data: "<< temp << "\n"; //DEBUG
+
+        if (temp < (float)100) {
+            data.push_back(temp);
+            ice++;
+        }
+        else if (temp > (float)167 && temp < (float)169) {
+            land++;
+        }
+        else if (temp > (float)156 && temp < (float)158) {
+            missing++;
+        }
+
+
+        total++;
+    }
+
+    for (auto &&i : data)
+    {
+        std::cout << "data:" << i << "\n";
 
     }
 
+    std::cout << "total:" << total << "\n";
+    std::cout << "ice:" << ice << "\n";
+    std::cout << "land:" << land << "\n";
+    std::cout << "missing:" << missing << "\n";
+
 
     file.close();
+
+}
+
+
+int main() {
+
+    std::vector<float> data;
+
+    
+    get_contents(data);
+
+
 
 
     return 0;
